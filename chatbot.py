1ceae2224e65017b005bcf313080c17d0ed76772
cbk914 <cbk914@riseup.net>
2023-03-31 22:23:10 +0200

    Update chatbot.py
    Separated the API key retrieval logic into a separate function.
    Removed the unnecessary argparse import.
    Improved exception handling.
    Added commented code to store api key in plaintext

--- chatbot.py
+++ chatbot.py
@@ -12,6 +12,7 @@ import dotenv
 
 # Set up the model (more models, visit https://beta.openai.com/docs/models/overview)
 APIKEY_FILENAME = ".env"
+# Max TOKEN_LIMIT 4087
 TOKEN_LIMIT = 4050
 MODEL_ENGINE = "gpt-3.5-turbo"
 
@@ -23,51 +24,44 @@ _(()_GPT CHAT BOT_\_V_
  \\                \\
 """)
 
-# Define a function that securely gets the API key:
-def get_api_key():
+# Define a function that securely gets the API key (more secure):
+def read_hashed_api_key():
     dotenv.load_dotenv()
-    try:
-        api_key_hashed = os.getenv("OPENAI_API_KEY_HASHED")
-        if api_key_hashed:
-            while True:
-                api_key = stdiomask.getpass("Please enter your OpenAI API key: ").strip()
-                hashed_api_key = hashlib.sha512(api_key.encode()).hexdigest()
-                if api_key_hashed == hashed_api_key:
-                    return api_key
-                else:
-                    print("Invalid API key. Please try again.")
-    except FileNotFoundError:
-        pass
+    return os.getenv("OPENAI_API_KEY_HASHED")
 
+
+def save_hashed_api_key(hashed_api_key):
+    dotenv.set_key(APIKEY_FILENAME, "OPENAI_API_KEY_HASHED", hashed_api_key)
+
+
+def ask_for_api_key():
     while True:
         api_key = stdiomask.getpass("Please enter your OpenAI API key: ").strip()
         hashed_api_key = hashlib.sha512(api_key.encode()).hexdigest()
-        dotenv.set_key(APIKEY_FILENAME, "OPENAI_API_KEY_HASHED", hashed_api_key)
-        return api_key
+        if read_hashed_api_key() == hashed_api_key:
+            return api_key
+        save_hashed_api_key(hashed_api_key)
+        print("Invalid API key. Please try again.")
+        
+# Define a function that stores the API key in plaintext (less secure) 
+# def read_api_key():
+#    dotenv.load_dotenv()
+#    return os.getenv("OPENAI_API_KEY")
+
+
+# def save_api_key(api_key):
+#    dotenv.set_key(APIKEY_FILENAME, "OPENAI_API_KEY", api_key)
+
+
+# def ask_for_api_key():
+#    api_key = stdiomask.getpass("Please enter your OpenAI API key: ").strip()
+#    if read_api_key() is None:
+#        save_api_key(api_key)
+#    return api_key
+
 
 # Set up the OpenAI API client using the secure method of getting the API key:
-if os.path.isfile(APIKEY_FILENAME):
-    with open(APIKEY_FILENAME, "r") as f:
-        api_key_hashed = f.readline().strip()
-        if api_key_hashed:
-            for i in range(3):
-                try:
-                    api_key = stdiomask.getpass("Please enter your OpenAI API key: ").strip()
-                except KeyboardInterrupt:
-                    exit()
-                hashed_api_key = hashlib.sha512(api_key.encode()).hexdigest()
-                if api_key_hashed == hashed_api_key:
-                    break
-                else:
-                    print("Invalid API key. Please try again.")
-            else:
-                print("Too many invalid attempts. Exiting program.")
-                exit()
-        else:
-            api_key = get_api_key()
-else:
-    api_key = get_api_key()
-
+api_key = ask_for_api_key()
 openai.api_key = api_key
 
 # Define a function that sends a message to ChatGPT:
@@ -89,14 +83,19 @@ def conversation_handler(prompt):
     response = chat_query(prompt)
     print(f"ChatGPT: {response}")
 
-    # End the conversation if ChatGPT says goodbye:
-    if "goodbye" in response.lower():
+    goodbye_phrases = [
+        "bye", "deu", "adios", "doei", "hasta la vista",
+        "hasta luego", "ciao", "agur", "adeus", "tchau"
+    ]    
+
+    # End the conversation if any of the specified expressions are included in the user input:
+    if any(phrase in prompt.lower() for phrase in goodbye_phrases):
+        print("You ended the conversation.")
         return
 
     # Otherwise, get user input and continue the conversation:
     prompt = input("You: ")
     conversation_handler(prompt)
-
 
 # Main program starts here:
 if __name__ == "__main__":
